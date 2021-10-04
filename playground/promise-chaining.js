d1ee6f8a40f1e697cb2032cd6ef349b650ec8755
Sathsara Warushawithana <sathsara@calcey.com>
2021-10-04 23:37:56 +0530

    Added Patch endpoints and optimized functionality

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,18 +1,43 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user.js');
-
+const Task = require('../src/models/tasks.js');
 // 615af6f59f7d978346952b05
 
-User.findByIdAndUpdate('615af396676ad6099c7142b6', {age: 1})
-.then(user =>{
-    console.log(user);
+// User.findByIdAndUpdate('615af396676ad6099c7142b6', {age: 1})
+// .then(user =>{
+//     console.log(user);
 
-    return User.countDocuments({age:1});
-})
-.then(result => {
-    console.log(result);
-})
-.catch(e => {
-    console.log(e);
-})
+//     return User.countDocuments({age:1});
+// })
+// .then(result => {
+//     console.log(result);
+// })
+// .catch(e => {
+//     console.log(e);
+// })
 
+
+// const updateAgeAndCount = async (id, age) => {
+//     const user = await User.findByIdAndUpdate(id, {age});
+
+//     const count = await User.countDocuments({age});
+
+//     return {
+//         previous_user_info: user,
+//         count
+//     }
+// }
+
+// updateAgeAndCount("615af396676ad6099c7142b6", 45)
+// .then(result => console.log(result))
+// .catch(e => console.log(e));
+
+const deleteTaskAndCount = async (id) =>{
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed:false});
+    return count;
+}
+
+deleteTaskAndCount("615b3a5b1b579c8329bd705b")
+.then(count => console.log(count))
+.catch(e => console.log(e));
--- src/index.js
+++ src/index.js
@@ -13,89 +13,154 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Create new user - POST
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 
     const user = new User(req.body);
-    user.save()
-        .then((result) => {
-            res.status(201).send(result);
-        })
-        .catch(error => {
-            res.status(400).send(error);
-        });
+
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 // Get a particular User
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
 
     const _id = req.params.id;
-    User.findById(_id)
-        .then(result => {
-            if (!result) {
-                return res.status(404).send();
-            }
-            res.send(result);
 
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        })
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 
 // Get All Users
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
 
-    User.find({}).then(users => {
+
+    try {
+
+        const users = await User.find({});
         res.send(users);
-    })
-        .catch(error => {
-            res.status(500).send(error);
-        })
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// PATCH - update a user
+app.patch('/users/:id', async (req, res) => {
+
+    const _id = req.params.id;
+
+    // making sure the updates are within the requirement.
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        // new: true will return the updated user
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
 });
 
 // Create new Task - POST
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
 
     const task = new Task(req.body);
-
-    task.save()
-        .then(result => {
-            res.status(201).send(result);
-        })
-        .catch(error => {
-            res.status(400).send(error);
-        })
+    try {
+        await task.save();
+        res.status(201).send(task);
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
 });
 
 // Get a particular task
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
 
     const _id = req.params.id;
-    Task.findById(_id)
-        .then(result => {
-            if(!result){
-                return res.status(404).send();
-            }
 
-            res.send(result);
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        });
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    }
+
+    catch (error) {
+        res.status(500).send(error);
+    }
 })
 
 //Get All Tasks
-app.get('/tasks', (req, res) => {
-    Task.find({})
-        .then(result => {
-            res.send(result);
-        })
-        .catch(error => {
-            res.status(500).send(error);
+app.get('/tasks', async (req, res) => {
 
-        });
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
 });
+
+// PATCH - update a task
+app.patch("/tasks/:id", async (req, res) => {
+
+    const _id = req.params.id;
+
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Updates' });
+    }
+
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true });
+
+        if (!updatedTask) {
+            return res.status(404).send();
+        }
+        res.send(updatedTask);
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is listening on port : ${port}`);
